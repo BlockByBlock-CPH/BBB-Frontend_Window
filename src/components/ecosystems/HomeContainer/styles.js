2c5517437db026b279827d1b9f3cc7204e465874
max-cl <maximiliano.arias@gmail.com>
2018-10-11 15:40:35 +0200

    #

---
+++ src/components/ecosystems/HomeContainer/styles.js
@@ -0,0 +1,104 @@
+const drawerWidth = 300;
+const drawerWidthRight = 300;
+
+export const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  appFrame: {
+    height: '100%',
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  appBar: {
+    position: 'absolute',
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    backgroundColor: '#000000'
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  'appBarShift-left': {
+    marginLeft: drawerWidth,
+  },
+  'appBarShift-right': {
+    marginRight: drawerWidthRight,
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+  },
+  drawerPaperRight: {
+    position: 'relative',
+    width: drawerWidthRight,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  drawerHeaderRight: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    backgroundColor: '#000000'
+  },
+  content: {
+    flexGrow: 1,
+    // backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    // padding: 10,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    })
+  },
+  'content-left': {
+    marginLeft: -drawerWidth,
+  },
+  'content-right': {
+    marginRight: -drawerWidthRight,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  'contentShift-left': {
+    marginLeft: 0,
+  },
+  'contentShift-right': {
+    marginRight: 0,
+  },
+  Logo: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '20%',
+    maxHeight: '20%',
+  },
+  Typography:{
+    color: '#FFFFFF'
+  }
+});
