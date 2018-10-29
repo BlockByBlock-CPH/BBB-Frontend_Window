a6c1af09bf01b51a1242561d026f51e69a654b75
max-cl <maximiliano.arias@gmail.com>
2018-10-29 13:08:16 +0100

    redux-reselect - selector - memoize

---
+++ src/redux/selectors/address_selector.js
@@ -0,0 +1,34 @@
+import { createSelector } from 'reselect';
+
+// selectors
+const getCoordAddress = (address) => address.selectedInfo.coordAddress;
+const getMainChart = (address) => address.mainChart;
+const getSelectedDay = (address) => address.selectedInfo.selectedDay;
+const getTableHome = (address) => address.tableHome;
+const getDataTop = (address) => address.dataTop;
+
+// reselect functions
+export const getCoordAddressState = createSelector(
+  [ getCoordAddress ],
+  (coordAddress) => coordAddress
+)
+
+export const getMainChartState = createSelector(
+  [ getMainChart ],
+  (mainChart) => mainChart
+)
+
+export const getSelectedDayState = createSelector(
+  [ getSelectedDay ],
+  (selectedDay) => selectedDay
+)
+
+export const getTableHomeState = createSelector(
+  [ getTableHome ],
+  (tableHome) => tableHome
+)
+
+export const getDataTopState = createSelector(
+  [ getDataTop ],
+  (dataTop) => dataTop
+)
